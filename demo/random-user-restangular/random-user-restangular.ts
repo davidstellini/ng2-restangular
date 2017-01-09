54a5bdd934408fb22b2c2ba0ca906cc55b01ba4e
David <dstellini@nxpay.eu>
2017-01-09 12:01:02 +0100

    refactor(demo): Remove demo
    Remove demo project.

--- demo/random-user-restangular/random-user-restangular.ts
+++
@@ -1,9 +0,0 @@
-import {OpaqueToken} from "@angular/core";
-import {Restangular} from "../../src/ng2-restangular";
-
-export const RANDOM_USER = new OpaqueToken('RandomUser');
-export function RestangularRandomUserFactory(restangular: Restangular) {
-  return restangular.withConfig((RestangularConfigurer) => {
-    RestangularConfigurer.setBaseUrl('https://randomuser.me/api/');
-  });
-}
